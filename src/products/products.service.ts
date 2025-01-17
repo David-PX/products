47d9c5c55cf3e3552e984bb37039065d3d88b324
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2025-01-17 08:37:08 -0400

    Products microservices initialized

---
+++ src/products/products.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as UuidV4 } from 'uuid';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  create(createProductDto: CreateProductDto) {
+    const { name, description, price } = createProductDto;
+
+    const newProduct = new Product(UuidV4(), name, description, price);
+
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: string) {
+    const product = this.products.find(id);
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
