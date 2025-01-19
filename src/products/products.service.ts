43c74d572e935796838638b6fedc4c77ce81bc63
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2025-01-18 22:51:53 -0400

    products microservice created

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as UuidV4 } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -21,17 +21,29 @@ export class ProductsService {
     return this.products;
   }
 
-  findOne(id: string) {
-    const product = this.products.find(id);
+  findOne(id: string): Product {
+    const product = this.products.find((product) => product.id == id);
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} no found`);
+    }
 
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    const { id: __, name, description, price } = updateProductDto;
+
+    const product = this.findOne(id);
+    product.updateWith({ name, description, price });
+
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string): Product {
+    const product = this.findOne(id);
+
+    this.products = this.products.filter((product) => product.id !== id);
+    return product;
   }
 }
